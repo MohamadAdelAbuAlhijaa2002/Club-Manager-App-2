77c7a944afca78abb53f2a756fc1db943c801cc3
DELL <muhammad.adel.abualhaijaa@gmail.com>
2025-11-28 01:09:47 +0300

    Add shorebird.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:club_app_organizations_section/appScreenOrganizations/notification/notificationScreen.dart';
 import 'package:club_app_organizations_section/saveToken/saveToken.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,6 +25,8 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
 
   final firebaseNotification = FirebaseNotification();
   await firebaseNotification.initNotifications();
