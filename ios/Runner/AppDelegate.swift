4ba9e7438456ff024c66c175019e985416186edc
DELL <muhammad.adel.abualhaijaa@gmail.com>
2025-11-28 20:41:35 +0300

    Update Firebase notifications, background handler, and iOS setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,34 +1,30 @@
-import Flutter
 import UIKit
-
+import Flutter
 import Firebase
 import FirebaseMessaging
 
-@UIApplicationMain
-
-
-@main
+@main // ✅ فقط هذا
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-      FirebaseApp.configure()
+    // تهيئة Firebase
+    FirebaseApp.configure()
 
-
-
+    // تسجيل الـ plugins
     GeneratedPluginRegistrant.register(with: self)
 
-
+    // تسجيل الإشعارات البعيدة
     application.registerForRemoteNotifications()
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-
-   override func application(_ application: UIApplication,
+  // الحصول على Device Token الخاص بـ APNs
+  override func application(_ application: UIApplication,
       didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      Messaging.messaging().apnsToken = deviceToken
-    }
+    Messaging.messaging().apnsToken = deviceToken
+  }
 }
