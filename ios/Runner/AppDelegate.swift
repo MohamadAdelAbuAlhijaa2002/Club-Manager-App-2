f61533f9f70f509c67fa66d0b5d52e8e88fdd28e
abwalhyja607 <abwalhyja607@gmail.com>
2025-12-01 05:46:40 +0300

    Initial commit - Flutter project with Firebase Notification setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,65 +3,43 @@ import Flutter
 import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
+    let channelName: String = "PushNotificationChannel"
+    var deviceToken: String = ""
 
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
 
-let channelName: String = "PushNotificationChannel"
-var deviceToken: String = ""
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        let pushNotificationChannel = FlutterMethodChannel(name: channelName, binaryMessenger: controller.binaryMessenger)
 
-override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-) -> Bool {
-
-    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-    let pushNotificationChannel = FlutterMethodChannel(name: channelName, binaryMessenger: controller.binaryMessenger)
-
-    if #available(iOS 10.0, *) {
-        UNUserNotificationCenter.current().delegate = self
-    }
-
-    pushNotificationChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
-        switch call.method {
-        case "requestNotificationPermissions":
-            self?.requestNotificationPermissions(result: result)
-        case "registerForPushNotifications":
-            self?.registerForPushNotifications(application: application, result: result)
-        case "retrieveDeviceToken":
-            self?.getDeviceToken(result: result)
-        default:
-            result(FlutterMethodNotImplemented)
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self // لا حاجة لتصريح البروتوكول هنا
         }
-    }
 
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-}
-
-private func requestNotificationPermissions(result: @escaping FlutterResult) {
-    if #available(iOS 10.0, *) {
-        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(options: options) { granted, error in
-            result(granted)
+        pushNotificationChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            switch call.method {
+            case "requestNotificationPermissions":
+                self?.requestNotificationPermissions(result: result)
+            case "registerForPushNotifications":
+                self?.registerForPushNotifications(application: application, result: result)
+            case "retrieveDeviceToken":
+                result(self?.deviceToken)
+            default:
+                result(FlutterMethodNotImplemented)
+            }
         }
-    } else {
-        result(false)
+
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceTokenData: Data) {
+        self.deviceToken = deviceTokenData.map { String(format: "%02.2hhx", $0) }.joined()
+        print("Device Token: \(self.deviceToken)")
+        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceTokenData)
     }
 }
 
-private func registerForPushNotifications(application: UIApplication, result: @escaping FlutterResult) {
-    application.registerForRemoteNotifications()
-    result(true)
-}
-
-private func getDeviceToken(result: @escaping FlutterResult) {
-    result(deviceToken)
-}
-
-override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    self.deviceToken = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-}
-
-
-}
