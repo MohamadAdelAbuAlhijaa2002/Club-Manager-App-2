592adf3e125c87e213c780425477c10bebf32647
abwalhyja607 <abwalhyja607@gmail.com>
2025-12-01 05:10:38 +0300

    Initial commit - Flutter project with Firebase Notification setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,39 +1,13 @@
-import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
+import UIKit
 
-@UIApplicationMain
-class AppDelegate: FlutterAppDelegate, MessagingDelegate {
-
+@main
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    // تهيئة Firebase
-    FirebaseApp.configure()
-
-    // تعيين delegate للـ Firebase Messaging
-    Messaging.messaging().delegate = self
-
-    // إعداد إشعارات المستخدم
-    UNUserNotificationCenter.current().delegate = self
-
-    // تسجيل الجهاز لاستقبال الإشعارات البعيدة
-    application.registerForRemoteNotifications()
-
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // إعادة تعريف دالة تسجيل الجهاز لاستقبال إشعارات APNs
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-
-  // استلام توكن FCM
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("iOS FCM Token: \(fcmToken ?? "")")
-      // يمكن هنا إرسال التوكن للسيرفر إذا أردت
   }
 }
