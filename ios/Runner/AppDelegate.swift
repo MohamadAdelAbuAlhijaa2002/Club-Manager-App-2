4109104e404ccb59f0bfb4b0c3cb26a9a4393e2d
abwalhyja607 <abwalhyja607@gmail.com>
2025-12-02 03:53:03 +0300

    Initial commit - Flutter project with Firebase Notification setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,6 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
 
-    FirebaseApp.configure() // ضروري
 
     GeneratedPluginRegistrant.register(with: self)
 
